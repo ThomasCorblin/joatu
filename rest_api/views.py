599e336d1fd2617f4d7af81e5680327e291cee7c
moileretour <thomas.corblin@gmail.com>
2018-04-09 15:44:08 -0400

    a

--- rest_api/views.py
+++ rest_api/views.py
@@ -277,7 +277,7 @@ class CreateProfileView(CreateAPIView):
             instance=request.data
             response = super(CreateProfileView, self).create(request, *args, **kwargs)
             lat_cal, lng_cal = coordinates_calculation('', instance.__getitem__('postal_code'), instance.__getitem__('city'), instance.__getitem__('country'))
-            #profile= Profile.objects.get(user=self.request.user)
+            profile= Profile.objects.get(user=self.request.user)
             #ProfileGeolocation.objects.create(profile = profile, lat=lat_cal, lng= lng_cal)
             return redirect('homepage')
         except:
