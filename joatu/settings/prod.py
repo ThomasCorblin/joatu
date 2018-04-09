4cec51a29929ed7d992197d4719cd336f478286a
moileretour <thomas.corblin@gmail.com>
2018-04-09 12:41:03 -0400

    STATIC_ROOT in base.py

--- joatu/settings/prod.py
+++ joatu/settings/prod.py
@@ -14,7 +14,6 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
