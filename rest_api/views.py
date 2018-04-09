bf138c09042b0faf6aa14e9710725f2191e4fa2a
moileretour <thomas.corblin@gmail.com>
2018-04-09 15:34:48 -0400

    a

--- rest_api/views.py
+++ rest_api/views.py
@@ -277,8 +277,8 @@ class CreateProfileView(CreateAPIView):
             instance=request.data
             lat_cal, lng_cal = coordinates_calculation('', instance.__getitem__('postal_code'), instance.__getitem__('city'), instance.__getitem__('country'))
             response = super(CreateProfileView, self).create(request, *args, **kwargs)
-            profile= Profile.objects.get(user=self.request.user)
-            ProfileGeolocation.objects.create(profile = profile, lat=lat_cal, lng= lng_cal)
+            #profile= Profile.objects.get(user=self.request.user)
+            #ProfileGeolocation.objects.create(profile = profile, lat=lat_cal, lng= lng_cal)
             return redirect('homepage')
         except:
             print('error')
