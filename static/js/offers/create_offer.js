6b7460d194bb97de879f5175e01ea34a0828f79e
moileretour <thomas.corblin@gmail.com>
2018-04-12 14:55:54 -0400

    remove alert + redirection Offer Creation Success

--- static/js/offers/create_offer.js
+++ static/js/offers/create_offer.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#submit_offer').click(function(e){
         $.post('/rest_api/offers/', $('#form_offer').serialize())
         .done(function(data){
-            alert('hello')
+            window.location.href = "/list_offers/";
         });
         e.preventDefault();
 
